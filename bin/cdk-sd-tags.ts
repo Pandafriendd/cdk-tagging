7019282d72f3cf287199ae8cd15c9927e8784a9c
EC2 Default User <ec2-user@ip-172-31-26-122.us-east-2.compute.internal>
2020-11-16 22:41:16 +0000

    upload source code

--- bin/cdk-sd-tags.ts
+++ bin/cdk-sd-tags.ts
@@ -1,7 +1,33 @@
 #!/usr/bin/env node
-import 'source-map-support/register';
-import * as cdk from '@aws-cdk/core';
-import { CdkSdTagsStack } from '../lib/cdk-sd-tags-stack';
+import "source-map-support/register";
+import * as cdk from "@aws-cdk/core";
+import { CdkSdTagsStack } from "../lib/cdk-sd-tags-stack";
 
 const app = new cdk.App();
-new CdkSdTagsStack(app, 'CdkSdTagsStack');
+
+const BobTag = {
+  Name: "Sygna backoffice backend",
+  Owner: "Oo Kang Zheng",
+  Team: "Sygna",
+  Product: "Sygna",
+  Group: "Node",
+  Application: "Backend",
+  Description: "CDK generated Back office backend",
+};
+
+const stack = new CdkSdTagsStack(app, "CdkSdTagsStack", {
+  env: {
+    account: process.env.CDK_DEFAULT_ACCOUNT,
+    region: process.env.CDK_DEFAULT_REGION,
+  },
+});
+
+Object.entries(BobTag).forEach(([k, v]) => {
+  cdk.Tag.add(stack, k, v);
+});
+
+Object.entries(BobTag).forEach(([k, v]) => {
+  cdk.Tag.remove(stack, k, {
+    includeResourceTypes: ["AWS::ServiceDiscovery::PrivateDnsNamespace"],
+  });
+});
--- lib/cdk-sd-tags-stack.ts
+++ lib/cdk-sd-tags-stack.ts
@@ -1,9 +1,27 @@
-import * as cdk from '@aws-cdk/core';
+import * as cdk from "@aws-cdk/core";
+
+import * as ec2 from "@aws-cdk/aws-ec2";
+import * as servicediscovery from "@aws-cdk/aws-servicediscovery";
 
 export class CdkSdTagsStack extends cdk.Stack {
   constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
+    const vpc = ec2.Vpc.fromLookup(this, "VPC", {
+      isDefault: true,
+    });
+
+    new ec2.SecurityGroup(this, "SecurityGroup", {
+      vpc,
+    });
+
+    const vpcNamespace = new servicediscovery.PrivateDnsNamespace(
+      this,
+      `BobNamespace`,
+      {
+        vpc,
+        name: "test",
+      }
+    );
   }
 }
